7667d354b459af4f211543740105e64a2aba6363
Jigden Shakya <jigdenshakya@gmail.com>
2026-03-25 22:23:03 +0600

    fix: add jest configuration and testing dependencies for frontend

--- JigdenShakya_02240343_DSO101_A1/frontend/jest.config.js
+++ JigdenShakya_02240343_DSO101_A1/frontend/jest.config.js
@@ -1,10 +1,18 @@
 const nextJest = require('next/jest');
-const createJestConfig = nextJest({ dir: './' });
+
+const createJestConfig = nextJest({
+  dir: './',
+});
 
 const customJestConfig = {
-  setupFilesAfterFramework: ['<rootDir>/jest.setup.js'],
+  // Fixed: changed from setupFilesAfterFramework to setupFilesAfterEnv
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    // Handle CSS imports
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
 };
 
 module.exports = createJestConfig(customJestConfig);
